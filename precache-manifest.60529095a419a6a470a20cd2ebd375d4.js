882899e50b14f6235749494244008429e6a37788
USER <mubashirnadeem952@gmail.com>
2022-05-17 10:26:03 +0500

    Updates

---
+++ precache-manifest.60529095a419a6a470a20cd2ebd375d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fac942beec11e492dd99",
+    "url": "/react_crud_project/static/css/main.35a8df76.chunk.css"
+  },
+  {
+    "revision": "fac942beec11e492dd99",
+    "url": "/react_crud_project/static/js/main.fac942be.chunk.js"
+  },
+  {
+    "revision": "8f880586178fd7af2623",
+    "url": "/react_crud_project/static/css/1.7f942ee3.chunk.css"
+  },
+  {
+    "revision": "8f880586178fd7af2623",
+    "url": "/react_crud_project/static/js/1.8f880586.chunk.js"
+  },
+  {
+    "revision": "1f4b7f8f98092c799a72",
+    "url": "/react_crud_project/static/js/runtime~main.1f4b7f8f.js"
+  },
+  {
+    "revision": "498d7ae0fc4b3c134004221ae3f4f7ff",
+    "url": "/react_crud_project/index.html"
+  }
+];
